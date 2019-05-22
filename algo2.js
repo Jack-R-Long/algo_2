ba7e4b5374513da751fdb46ba406649cf665e2ea
Jack Long <jack.richard.long@gmail.com>
2019-05-22 11:35:31 -0700

    first commit

---
+++ algo2.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JS Bin</title>
+</head>
+<body>
+  <script type="javascript">
+   
+  function multiply(x,y){
+    console.log(x);
+    console.log(y);
+  }
+  b = multiply(2,3);
+  console.log(b);
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </script>
+</body>
+</html>
